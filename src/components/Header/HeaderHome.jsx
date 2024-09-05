82680819676a8512594436b4c28c6b708e145927
jaironf <jaironf.94@gmail.com>
2024-09-05 21:15:50 +0200

    Cambios en HeaderHome

--- src/components/Header/HeaderHome.jsx
+++ src/components/Header/HeaderHome.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Box } from '@chakra-ui/react'
+
 
 const HeaderHome = () => {
   return (
-    <div>HeaderHome</div>
+    <Box className='header-home'>HeaderHome
+        <Box className='title-home-nav'>J<i>a</i>ir<i>o</i> Nú<i>ñ</i>ez</Box>
+        <Box className='btn-home-nav'></Box>
+    </Box>
   )
 }
 
--- src/views/Portfolio/Porfolio.jsx
+++ src/views/Portfolio/Porfolio.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Box } from '@chakra-ui/react'
 import HeaderHome from '../../components/Header/HeaderHome'
+import './Portfolio.scss'
 
 const Porfolio = () => {
   return (
-   <Box className=''> BODY
+   <Box className='body'> BODY
         <HeaderHome/>
    </Box>
   )
