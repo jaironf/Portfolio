ab5335005ceb62b5899279c126947afa8ca8fe97
jaironf <jaironf.94@gmail.com>
2024-09-06 18:55:48 +0200

    HeaderHome terminado

--- src/components/Header/HeaderHome.jsx
+++ src/components/Header/HeaderHome.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
-import { Box, Divider } from '@chakra-ui/react'
+import { Box, Divider, Text } from '@chakra-ui/react'
 
 
 
 const HeaderHome = () => {
+  const currentDate = new Date();
+
+  const formattedDated = currentDate.toLocaleDateString('es-ES', {
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric',
+  });
+
+  const formattedTime = currentDate.toLocaleTimeString('es-ES', {
+    hour: '2-digit',
+    minute: '2-digit', 
+    second: '2-digit'
+  });
+
   return (
     <Box className='header-home'>
         <Box className='title-home-nav'>J<i>a</i>ir<i>o</i> <span className='space-between'>Nú<i>ñ</i>ez</span></Box>
         <Divider className='divider'/>
-        <Box className='btn-home-nav'></Box>
+        <Box className='subtitle-home-nav'>
+          <Text className='fullstack-text'>FULL-STACK DEVELOPER</Text>
+          <Text className='date-display'>{formattedDated}</Text>
+          <Text className='email-text'>EMAIL</Text>
+          <Text className='time-display'>{formattedTime}</Text>
+        </Box>
     </Box>
   )
 }
