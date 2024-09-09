d2c5b7436c17e3eb5cb93f8a77e0732a3ddb4ce2
jaironf <jaironf.94@gmail.com>
2024-09-09 20:33:25 +0200

    Imagen y descripción integradas

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Box, Divider, Text } from '@chakra-ui/react'
+import { Box, Divider, Image, Text } from '@chakra-ui/react'
+import './AboutMe-Pic.scss'
+
 
 const AboutMe = () => {
   return (
@@ -7,6 +9,14 @@ const AboutMe = () => {
         <Box className='title-about-nav'>
             <Text><i>A</i>bo<i>u</i>t <span className='space-between'><i>M</i>e</span></Text>
             <Divider className='divider'/>
+        </Box>
+        <Box className='pic-container'>
+            <Box className='profile-pic'>
+                <Image src='src/assets/Profile-pic.png' alt='profile-pic'/>
+            </Box>
+            <Box className='description-container'>
+              <Text className='description'>Hola caramierda</Text>
+            </Box>
         </Box>
     </Box>
   )
