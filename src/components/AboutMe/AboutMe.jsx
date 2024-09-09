3d130fa837991b766de0c607e30a9f59b3ce5ee2
jaironf <jaironf.94@gmail.com>
2024-09-09 21:10:33 +0200

    AboutMe terminado

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-import { Box, Divider, Image, Text } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { Box, Button, Divider, Image, Text } from '@chakra-ui/react'
 import './AboutMe-Pic.scss'
 
 
 const AboutMe = () => {
+  const [isEnglish, setIsEnglish] = useState(true);
+
+  const textInEnglish = 'I am a graduate in Tourism who has recently made a career shift by pursuing Fullstack Development. My passion for building websites inspired this change, and after completing my studies, I am now a junior developer with a strong desire to demonstrate my skills. I am currently seeking an opportunity to apply everything I have learned and grow professionally in the tech industry.';
+
+  const textInSpanish = 'Soy graduado en Turismo y he dado un giro a mi carrera al estudiar desarrollo Fullstack. Mi pasión por crear sitios web inspiró este cambio, y después de completar mis estudios, ahora soy un desarrollador junior con muchas ganas de demostrar mis habilidades. Actualmente estoy buscando una oportunidad para aplicar todo lo que he aprendido y crecer profesionalmente en la industria tecnológica.';
+
+  const toggleLanguage = () => {
+    setIsEnglish(!isEnglish)
+  }
+
+
   return (
     <Box className='aboutme-continer'>
         <Box className='title-about-nav'>
@@ -15,7 +26,9 @@ const AboutMe = () => {
                 <Image src='src/assets/Profile-pic.png' alt='profile-pic'/>
             </Box>
             <Box className='description-container'>
-              <Text className='description'>Hola caramierda</Text>
+              <Text className='description'>{isEnglish ? textInEnglish: textInSpanish}</Text>
+
+              <Button className='language-toggle-btn' colorScheme='yellow' onClick={toggleLanguage}>{isEnglish ? 'Cambiar a Español' : 'Switch to English' }</Button>
             </Box>
         </Box>
     </Box>
