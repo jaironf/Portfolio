83f603a8ec83b8d66920c018a72d7b9694b02cbd
jaironf <jaironf.94@gmail.com>
2024-09-10 18:43:38 +0200

    Cambio en formato de archivo Projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,14 +4,14 @@ import './Projects.scss'
 const Projects = () => {
   return (
     <Box className='projects-container'>
-        <Box className='selected-box'>
-            <Text className='selected-title'>S<i>e</i>lec<i>t</i>e<i>d</i></Text>
-            <Divider className='divider-body'/> 
-        </Box>
-        <Box className='projects-box'>
-            <Text className='projects-title'><i>P</i>ro<i>j</i>ec<i>t</i>s</Text>
-            <Divider className='divider-body'/> 
-        </Box>
+      <Box className='selected-box'>
+        <Text className='selected-title'>S<i>e</i>lec<i>t</i>e<i>d</i></Text>
+        <Divider className='divider-body' />
+      </Box>
+      <Box className='projects-box'>
+        <Text className='projects-title'><i>P</i>ro<i>j</i>ec<i>t</i>s</Text>
+        <Divider className='divider-body' />
+      </Box>
     </Box>
   )
 }
