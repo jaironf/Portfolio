e84ac4ad9ba33e21d9ec733a93e2f4abbc32ad56
jaironf <jaironf.94@gmail.com>
2024-09-14 12:56:11 +0200

    Cambios realizados

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -16,18 +16,18 @@ const Projects = () => {
       </Box>
         <Box className='project-cards'>
           <Grid h='500px' w='500px' templateRows='repeat (3, 1fr)' templateColumns='(6, 1fr)' gap={0}>
-            <GridItem rowSpan={2} colSpan={2} bg='#3E3C36' color={'#FBF0DF'} borderRadius='0 0 50px 0'>PROJECT <span>01</span></GridItem>
+            <GridItem className='grid-item' rowSpan={2} colSpan={2} bg='#3E3C36' color={'#FBF0DF'} borderRadius='0 0 50px 0'>PROJECT <span>01</span></GridItem>
           </Grid>
           <Grid h='500px' w='500px' templateRows='repeat (3, 1fr)'>
-            <GridItem rowSpan={2} bg='#E7A92D' borderRadius='0 0 50px 0'>PROJECT <span>02</span></GridItem>
+            <GridItem className='grid-item' rowSpan={2} bg='#E7A92D' borderRadius='0 0 50px 0'>PROJECT <span>02</span></GridItem>
           </Grid>
         </Box>
         <Box className='project-cards'>
           <Grid h='500px' w='770px' templateRows='repeat (3, 1fr)'>
-            <GridItem rowSpan={2} bg='#DB4C44' color={'#FBF0DF'} borderRadius='0 0 50px 0'>PROJECT <span>03</span></GridItem>
+            <GridItem className='grid-item' bg='#DB4C44' color={'#FBF0DF'} borderRadius='0 0 50px 0'>PROJECT <span>03</span></GridItem>
           </Grid>
           <Grid h='500px' w='600px' templateRows='repeat (3, 1fr)'>
-            <GridItem rowSpan={2} bg='#FBF0DF' borderRadius='0 0 50px 0'>PROJECT <span>04</span></GridItem>
+            <GridItem className='grid-item' rowSpan={2} bg='#FBF0DF' borderRadius='0 0 50px 0'><Text className='text-projects'>PROJECT <span>04</span></Text></GridItem>
           </Grid>
         </Box>
       </Box>
