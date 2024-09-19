291eee707ed0eb8a917803d7e3e2b11756258f39
jaironf <jaironf.94@gmail.com>
2024-09-19 20:03:40 +0200

    Cambio

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -22,7 +22,7 @@ const AboutMe = () => {
         </Box>
         <Box className='pic-container'>
             <Box className='profile-pic'>
-                <Image src='https://imgur.com/HhAt0fK' alt='profile-pic'/>
+                <Image src='https://imgur.com/a/SjM9Fxz' alt='profile-pic'/>
             </Box>
             <Box className='description-container'>
               <Text className='description'>{isEnglish ? textInEnglish : textInSpanish}</Text>
