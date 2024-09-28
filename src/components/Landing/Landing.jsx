d43f5576a2ddceb4a9d081c2d830376a8941fdb6
jaironf <jaironf.94@gmail.com>
2024-09-28 20:07:26 +0200

    Cambio en posicion de mensaje bienvenida

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -5,7 +5,7 @@ import { Box, Divider, Image, Text } from '@chakra-ui/react'
 const Landing = () => {
   const translations = ['HOLA', 'KAIXO', 'HELLO', 'BONJOUR', 'CIAO', 'HALLO', 'OLÁ', 'नमस्ते', 'こんにちは', 'Привет', '你好']
 
-  const [ currentLanguageIndex, setCurrentLanguageIndex ] = useState(0)
+  const [currentLanguageIndex, setCurrentLanguageIndex] = useState(0)
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -19,30 +19,33 @@ const Landing = () => {
   return (
     <Box className='landing-continer'>
       <Box className='landing-logo'>
-          <Image src='/Logo.png' alt='logo' boxSize='260px' />
+        <Image src='/Logo.png' alt='logo' boxSize='260px' />
       </Box>
-        <Box className='landing-box1'>
-          <a href='#AboutMe'>
-            <Box className='card-about'>About <span className='space-between'>①</span></Box>
-            </a>
-            <Text className='hello'>{translations[currentLanguageIndex]}</Text> 
-            <a href='#Projects'>
-            <Box className='projects-card'>Projects <span className='space-between'>②</span></Box>
-            </a>
-          <Divider className='divider-body'/>
-        </Box>
-        <Box className='landing-box2'>
-          <Text className='text-fullstack'>FullStack <span className='space-between'>Developer</span></Text>
-          <Divider className='divider-body'/>
-        </Box>
-        <Box className='landing-box3'>
-          <Text className='based-text'>Based</Text>
-          <a href='#Contact'>
+      <Box className='hello-mobil-container' >
+        <Text className='hello-mobil'>{translations[currentLanguageIndex]}</Text>
+      </Box>
+      <Box className='landing-box1'>
+        <a href='#AboutMe'>
+          <Box className='card-about'>About <span className='space-between'>①</span></Box>
+        </a>
+        <Text className='hello'>{translations[currentLanguageIndex]}</Text>
+        <a href='#Projects'>
+          <Box className='projects-card'>Projects <span className='space-between'>②</span></Box>
+        </a>
+        <Divider className='divider-body' />
+      </Box>
+      <Box className='landing-box2'>
+        <Text className='text-fullstack'>FullStack <span className='space-between'>Developer</span></Text>
+        <Divider className='divider-body' />
+      </Box>
+      <Box className='landing-box3'>
+        <Text className='based-text'>Based</Text>
+        <a href='#Contact'>
           <Box className='card-contact'>Contact <span className='space-between'>③</span></Box>
-          </a>
-          <Text className='based-text'>in Valencia</Text>
-          <Divider className='divider-body'/>
-        </Box>
+        </a>
+        <Text className='based-text'>in Valencia</Text>
+        <Divider className='divider-body' />
+      </Box>
     </Box>
   )
 }
